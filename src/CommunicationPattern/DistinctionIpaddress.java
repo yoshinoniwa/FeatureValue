9dc1c721b065b4bd8746599d3c62e19417e812af
yoshinoniwa <yoshinoniwa>
2018-08-08 09:29:06 +0900

    distinctionIpaddress追加

--- src/CommunicationPattern/DistinctionIpaddress.java
+++ src/CommunicationPattern/DistinctionIpaddress.java
@@ -8,9 +8,26 @@ public class DistinctionIpaddress {
 	//IPアドレスごとにリストを作る
 	//------------------------------------------
 	//変数宣言
-	ArrayList<String> ipAddress_list = new ArrayList<String>();
+	ArrayList<String> ipAddress_list = new ArrayList<String>(); //Ipアドレスを格納するリスト
 	//ログセット
 	public void setLog(String log[][]){
 		System.out.println("DistinctionIpaddress  "+log.length);
+		separateIpaddress(log);
+	}
+	
+	public void separateIpaddress(String log[][]){
+		for(int i=0;i<log.length;i++){
+			if(i>1){//最初の一行無視
+				//ログのIPアドレスとリストに入っているIPアドレスが違えばIPアドレスをリストに入れる
+				for(int j=0;j<ipAddress_list.size();j++){
+					if(!log[i][3].equals(ipAddress_list.get(j))){
+						ipAddress_list.add(log[i][3]);
+						}else{}
+					}
+				
+			}
+			
+		}
+		
 	}
 }
